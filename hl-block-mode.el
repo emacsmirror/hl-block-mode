f957c01cf841072da43bf7736636d540f1763bfd
Campbell Barton <ideasman42@gmail.com>
2019-04-30 10:11:31 +1000

    Docs: add missing sections to header

--- hl-block-mode.el
+++ hl-block-mode.el
@@ -1,13 +1,36 @@
-;;; hl-block-mode.el --- minor mode for highlighting blocks
+;;; hl-block-mode.el --- highlighting nested blocks -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Campbell Barton
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
+
+;; URL: https://github.com/ideasman42/emacs-hl-block-mode
 ;; Version: 0.1
+;; Package-Requires: ((emacs "26.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; TODO.
-;; - More control of alpha blending.
-;; - Options to highlight bracket types besides '{}'.
+;; Highlight blocks surrounding the cursor.
+
+;;; Usage
+
+;; (hl-block-mode)
+;;
+;; Currently only curly braces are supported (C-family languages).
+
 
 ;;; Code:
 
